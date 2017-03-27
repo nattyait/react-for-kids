57c083a20f4897c5fdc220ce3aac19a4870fdf8f
nattyait <nattanicha@gmail.com>
2017-03-28 00:36:28 +0700

    Addion + 1, Addtion + 10, Alphabet :baby:

---
+++ README.md
@@ -0,0 +1,6 @@
+# React App for Kids
+Credit: Chawin Phatharamalai, my little tester
+## Feature
+- Addition + 1
+- Addition + 10
+- Alphabet A-Z
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PlusOneButton from './PlusOneButton'
+import PlusTenButton from './PlusTenButton'
+import RandomColorButton from './RandomColorButton'
+
+const App = () => (
+  <div>
+    <PlusOneButton />
+    <PlusTenButton />
+    <RandomColorButton />
+  </div>
+)
+
+export default App
